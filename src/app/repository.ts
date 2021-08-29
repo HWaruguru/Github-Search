53d00412c54eda75b760341d829340d722c34424
Waruguru <hannah.waruguru8@gmail.com>
2021-08-29 07:53:48 +0300

    Define the user and the repository classes

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,12 @@
 export class Repository {
+    constructor(
+        name: string,
+        description: string,
+        stargazers_count: string,
+        language: string,
+        open_isssue: string,
+        homepage: string,
+        created_by: string,
+        url: string
+    ) {}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,15 @@
 export class User {
+    constructor(
+        username: string,
+        email: string,
+        location: string,
+        created_at: string,
+        followers: string,
+        public_repos: string,
+        public_gists: string,
+        bio: string,
+        name?: string,
+    ) {}
 }
+
+
