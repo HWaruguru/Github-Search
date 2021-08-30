75bea114395e6b9ccc1327a876ca627e111fb817
Waruguru <hannah.waruguru8@gmail.com>
2021-08-30 16:43:08 +0300

    remove envs from git

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  APIKEY: "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",
-
-  apiUrl: "https://api.github.com",
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-  production: false,
-  APIKEY: "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"
-};
-
